f2251b392255eb2d2f1c574abfaa12fd9722fde8
yshiyi <s268yang@uwaterloo.ca>
2020-11-18 01:38:36 -0500

    Update 977. Squares of a Sorted Array.py

--- Easy/Array/977. Squares of a Sorted Array.py
+++ Easy/Array/977. Squares of a Sorted Array.py
@@ -1,5 +1,6 @@
 '''
 977. Squares of a Sorted Array
+Array, Two Pointers
 
 Description:
 Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
