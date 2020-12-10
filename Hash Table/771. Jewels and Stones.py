2fc3ea8f63fc022e19291708e4261694e4e24352
yshiyi <s268yang@uwaterloo.ca>
2020-12-10 02:16:10 -0500

    Create 771. Jewels and Stones.py

---
+++ Hash Table/771. Jewels and Stones.py
@@ -0,0 +1,36 @@
+'''
+771. Jewels and Stones
+Hash Table
+
+Description:
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  
+Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+The letters in J are guaranteed distinct, and all characters in J and S are letters. 
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+
+Example 2:
+Input: J = "z", S = "ZZ"
+Output: 0
+'''
+
+# Solution
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        
+        '''
+        Method: Check each stone, if it is a jewel.
+        '''
+        num = 0
+        for s in S:
+            if s in J:
+                num += 1
+        return num
