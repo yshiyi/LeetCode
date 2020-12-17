14e3d83205975d1d8505c2d5a4ada044c69fa3e9
yshiyi <s268yang@uwaterloo.ca>
2020-12-17 03:45:09 -0500

    Update 387. First Unique Character in a String.py

--- Hash Table/387. First Unique Character in a String.py
+++ Hash Table/387. First Unique Character in a String.py
@@ -47,3 +47,15 @@ class Solution(object):
             return min(Unseen.values())
         else:
             return -1
+        
+        
+        """
+        Method 2: build hash map : character and how often it appears
+        """
+        count = collections.Counter(s)
+        
+        # find the index
+        for idx, ch in enumerate(s):
+            if count[ch] == 1:
+                return idx     
+        return -1
