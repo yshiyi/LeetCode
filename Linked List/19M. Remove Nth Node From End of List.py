2e0aaac439ffded0861f78bf8420554b8f083bf8
yshiyi <s268yang@uwaterloo.ca>
2020-12-22 04:14:08 -0500

    Update 19M. Remove Nth Node From End of List.py

--- Linked List/19M. Remove Nth Node From End of List.py
+++ Linked List/19M. Remove Nth Node From End of List.py
@@ -1,5 +1,6 @@
 """
 19. Remove Nth Node From End of List
+Linked List, Two Pointers
 
 Description:
 Given the head of a linked list, remove the nth node from the end of the list and return its head.
