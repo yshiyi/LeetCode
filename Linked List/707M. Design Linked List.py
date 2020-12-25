eafe1af58c95ddbcd5985ecaf50ae30479d9f7fe
yshiyi <s268yang@uwaterloo.ca>
2020-12-25 04:18:06 -0500

    Update 707M. Design Linked List.py

--- Linked List/707M. Design Linked List.py
+++ Linked List/707M. Design Linked List.py
@@ -200,6 +200,108 @@ class MyLinkedList(object):
         self.size -= 1
 
 
+############################################################################################
+############################ Doubly Linked List ############################################
+############################################################################################
+class ListNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+
+class MyLinkedList(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.head = ListNode(None)
+        self.length = 0
+
+    def get(self, index):
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        :type index: int
+        :rtype: int
+        """
+        if index < 0 or index >= self.length:
+            return -1
+
+        cur = self.head
+
+        for _ in range(index):
+            cur = cur.next
+
+        return cur.val
+
+    def addAtHead(self, val):
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        :type val: int
+        :rtype: None
+        """
+        self.addAtIndex(0, val)
+
+    def addAtTail(self, val):
+        """
+        Append a node of value val to the last element of the linked list.
+        :type val: int
+        :rtype: None
+        """
+        self.addAtIndex(self.length, val)
+
+    def addAtIndex(self, index, val):
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        :type index: int
+        :type val: int
+        :rtype: None
+        """
+        if index > self.length:
+            return
+
+        newNode = ListNode(val)
+        cur = self.head
+        if index <= 0:
+            cur.prev = newNode
+            newNode.next = cur
+            self.head = newNode
+        else:
+            for _ in range(index):
+                cur = cur.next
+            temp = cur.prev
+            cur.prev = newNode
+            newNode.next = cur
+            temp.next = newNode
+            newNode.prev = temp
+
+        self.length += 1
+
+    def deleteAtIndex(self, index):
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        :type index: int
+        :rtype: None
+        """
+        if index < 0 or index >= self.length:
+            return -1
+
+        current = self.head
+        if index == 0:
+            self.head = current.next
+            self.head.prev = None
+        else:
+            for _ in range(index):
+                current = current.next
+            temp1 = current.prev
+            temp2 = current.next
+            temp1.next = current.next
+            temp2.prev = current.prev
+
+        self.length -= 1
+        
+
 
 obj = MyLinkedList()
 param_1 = obj.get(1)
@@ -211,7 +313,4 @@ print(obj.get(0), obj.get(1), obj.get(2))  # 1, 2, 3
 obj.deleteAtIndex(1)
 print(obj.get(0), obj.get(1), obj.get(2))  # 1, 3
 
-
-# MyLinkedList = LinkedList()
-# print(MyLinkedList)
 
