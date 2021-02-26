5596fa4fd63ed622a2dd73dc0b52342039641133
yshiyi <s268yang@uwaterloo.ca>
2021-02-26 01:02:52 -0500

    Create 189M. Rotate Array.cpp

---
+++ Array/189M. Rotate Array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+        // Method 1: using reverse(nums.begin(), nums.end())
+        k %= nums.size();
+        vector<int>::iterator it = nums.begin();
+        for (int i=0; i<k; i++) {
+            it++;
+        }
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), it);
+        reverse(it--, nums.end());
+        
+        
+        /* Method 2: Create an extra vector
+                     Elements will be moved to (i+k)%nums.size() position.
+        */
+        k %= nums.size();
+        vector<int> nums2;
+        int l = nums.size();
+        nums2.resize(l);
+        for (int i=0; i<l; i++) {
+            nums2[(i+k)%l] = nums[i];
+        }
+        nums.clear();
+        for (auto v:nums2) {
+            nums.push_back(v);
+        }
+    }
+};
