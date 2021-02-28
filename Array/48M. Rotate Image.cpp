b71e87099215a26c6193245fd7a1af5f78cd2e9f
yshiyi <s268yang@uwaterloo.ca>
2021-02-28 00:41:40 -0500

    Create 48M. Rotate Image.cpp

---
+++ Array/48M. Rotate Image.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int l = matrix.size(), temp;
+        for (int i=0; i<l/2; i++) {
+            for (int j=0+i; j<l-i-1; j++) {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[l-j-1][i];
+                matrix[l-j-1][i] = matrix[l-i-1][l-j-1];
+                matrix[l-i-1][l-j-1] = matrix[j][l-i-1];
+                matrix[j][l-i-1] = temp;
+            }
+        }
+    }
+};
