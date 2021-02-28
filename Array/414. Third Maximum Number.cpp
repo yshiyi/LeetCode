9e40c45671ae3184a6aa620021b1a181f0d30a73
yshiyi <s268yang@uwaterloo.ca>
2021-02-27 22:53:57 -0500

    Update 414. Third Maximum Number.cpp

--- Array/414. Third Maximum Number.cpp
+++ Array/414. Third Maximum Number.cpp
@@ -2,7 +2,11 @@ class Solution {
 public:
     int thirdMax(vector<int>& nums) {
         
-        // Method: using vector<int>::reverse_iterator rit = nums.rbegin(), rit++ goes from right to left
+        /* Method: using vector<int>::reverse_iterator rit = nums.rbegin(), rit++ goes from right to left
+                   Note: 1. We can't access to nums.rend(), so we terminate the loop at the second element
+                         2. Using *(it+1) to access next element. Using *(it++), it will automatically increase by 1.
+                         3. Return next element when count == 2
+        */
         if (nums.size() > 2) {
             sort(nums.begin(), nums.end());
             int count = 0;
