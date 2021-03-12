e28823afac7cb73b231316cd0fb43831e3dc8191
yshiyi <s268yang@uwaterloo.ca>
2021-03-12 00:45:30 -0500

    Create 347M. Top K Frequent Elements.cpp

---
+++ Hash Table/347M. Top K Frequent Elements.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    static bool myCompare(pair<int, int>& p1, pair<int, int>& p2) {
+        return p1.second > p2.second;
+    }
+    
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        /* Method 1: First, use a map to count the appearances of each unique element,
+                     Then, use a multimap to flip the key and value in map.
+                     Finally, save the last k elements to a vector.
+        */
+        map<int, int> m;
+        for (auto& val:nums) {
+            if (m.find(val) != m.end()) {
+                m[val]++;
+            }else {
+                m.insert(make_pair(val, 1));
+            }
+        }
+        
+        multimap<int, int> mm;
+        for (auto& key:m) {
+            mm.insert(make_pair(key.second, key.first));
+        }
+        
+        vector<int> res;
+        multimap<int, int>::reverse_iterator it = mm.rbegin();
+        for (int i=0; i<k; ++i) {
+            res.push_back(it->second);
+            it++;
+        }
+        
+        return res;
+        
+        
+        /* Method 2: First, create a map. 
+                     The kay is the unique elements, the value is the currect size of vecValues.
+                     Create a vector of pairs of integers. The index of the vector is the value of the map.
+                     The first value of the pair is the unique element, and the second value of the pair is the number of appearances.
+                     Then sort this vector using self-defined functional object (it must be static).
+                     Finally, save the first k elements to vecAnswer.
+        */
+        unordered_map<int, int> mapValues;
+        vector<pair<int, int>> vecValues;
+        vector<int> vecAnswer;
+        
+        for(auto& val:nums)
+        {
+            if(mapValues.find(val) != mapValues.end())
+            {
+                vecValues[mapValues[val]].second++;
+            }
+            else
+            {
+                mapValues[val] = vecValues.size();
+                vecValues.emplace_back(pair<int,int>{val, 1});
+            }
+        }
+        
+        sort(vecValues.begin(), vecValues.end(), myCompare);
+        for(int nIndex = 0; nIndex < k; nIndex++)
+        {
+            vecAnswer.push_back(vecValues[nIndex].first);
+        }
+        return vecAnswer;
+        
+        
+    }
+};
