ed1219f42a78f24020221590ad9df39de2a95937
yshiyi <s268yang@uwaterloo.ca>
2021-03-22 00:19:31 -0400

    Update 219. Contains Duplicate II.cpp

--- Hash Table/219. Contains Duplicate II.cpp
+++ Hash Table/219. Contains Duplicate II.cpp
@@ -1,22 +1,26 @@
+/* Method 1: Using hash table
+             The key is the element in nums, and the value is the index of the element in nums.
+             We find out the minimum distance between two duplicate elements.
+             In the end, we check if the minimum distance is less than k.
+*/
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         // Note: set the initial value of min_dis 
         //       as allowed maximum value of k
-        int min_dis = pow(10, 5), dis;
+        int min_dis = INT_MAX;
         map<int, int> m;
         map<int, int>::iterator it;
         for (unsigned int i=0; i<nums.size(); ++i) {
             it = m.find(nums[i]);
             if (it == m.end()) {
-                m.insert(make_pair(nums[i], i));
+                //m.insert(make_pair(nums[i], i));
+                m[nums[i]] = i;
             }else {
-                dis = i - (it)->second;
+                int dis = i - (it)->second;
                 if (dis <= min_dis) {
                     min_dis = dis;
                     m[nums[i]] = i;
-                }else {
-                    continue;
                 }
             }
         }
@@ -26,5 +30,31 @@ public:
         }else {
             return false;
         }
+    }
+};
+
+
+/* Method 2: Sliding Window
+*/
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        map<int, int> m;
+        int right = 0, left = 0;
+        while(right < nums.size()){
+            int val = nums[right];
+            if(m.find(val)==m.end()){
+                m[val] = right;
+            }else if(right - m[val] <= k){
+                return true;
+            }
+            right++;
+            
+            if(right - left > k){
+                m.erase(nums[left]);
+                left++;
+            }
+        }
+        return false;
     }
 };
