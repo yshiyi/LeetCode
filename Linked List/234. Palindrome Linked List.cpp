1ec314429139a4321eb6ba0655a6f4017f2652ab
yshiyi <s268yang@uwaterloo.ca>
2021-04-04 00:32:44 -0400

    Update 234. Palindrome Linked List.cpp

--- Linked List/234. Palindrome Linked List.cpp
+++ Linked List/234. Palindrome Linked List.cpp
@@ -35,3 +35,28 @@ public:
 };
 
 // Method 2: Using reverse approach
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        ListNode *head2=nullptr, *temp;
+        while(fast!=NULL&&fast->next!=NULL){
+            fast = fast->next->next;
+            temp = slow->next;
+            slow->next = head2;
+            head2 = slow;
+            slow = temp;
+        }
+        if(fast!=NULL){
+            slow = slow->next;
+        }
+        while(head2!=NULL&&slow!=NULL){
+            if(head2->val != slow->val){
+                return false;
+            }
+            head2 = head2->next;
+            slow = slow->next;
+        }
+        return true;
+    }
+};
