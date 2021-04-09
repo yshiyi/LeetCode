982b972d0de26abe0becbeecaf0b6fb924edd4d1
yshiyi <s268yang@uwaterloo.ca>
2021-04-09 00:26:53 -0400

    Create 561. Array Partition I.cpp

---
+++ String/561. Array Partition I.cpp
@@ -0,0 +1,37 @@
+/*
+561. Array Partition I
+Array
+
+Description:
+Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+
+
+Example 1:
+Input: nums = [1,4,3,2]
+Output: 4
+Explanation: All possible pairings (ignoring the ordering of elements) are:
+1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+So the maximum possible sum is 4.
+
+Example 2:
+Input: nums = [6,2,6,5,1,2]
+Output: 9
+Explanation: The optimal pairing is (2, 1), (2, 5), (6, 6). min(2, 1) + min(2, 5) + min(6, 6) = 1 + 2 + 6 = 9.
+
+*/
+
+// Method:  Minimum element gets add into the result in sacrifice of maximum element.
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+        for(int i=1;i<nums.size();i+=2){
+            sum += min(nums[i], nums[i-1]);
+        }
+        return sum;
+    }
+};
