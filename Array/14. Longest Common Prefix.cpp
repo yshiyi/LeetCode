cc316c8dff01abffd87e20e81784dbc752769ba5
yshiyi <s268yang@uwaterloo.ca>
2021-04-12 11:32:28 -0400

    Update 14. Longest Common Prefix.cpp

--- Array/14. Longest Common Prefix.cpp
+++ Array/14. Longest Common Prefix.cpp
@@ -28,7 +28,7 @@ public:
         string ans = "";
         for (int i=0; i< strs[0].size(); i++){
             for(int j=1; j<strs.size();j++){
-                if(strs[0][i]!=strs[j][i]){
+                if(i==strs[j].size() || strs[0][i]!=strs[j][i]){
                     return ans;
                 }
             }
