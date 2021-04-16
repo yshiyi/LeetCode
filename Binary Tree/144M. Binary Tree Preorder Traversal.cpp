9908011cc4af9996f8b4a38618cafd9e7b2b3514
yshiyi <s268yang@uwaterloo.ca>
2021-04-16 00:41:32 -0400

    Update 144M. Binary Tree Preorder Traversal.cpp

--- Binary Tree/144M. Binary Tree Preorder Traversal.cpp
+++ Binary Tree/144M. Binary Tree Preorder Traversal.cpp
@@ -88,3 +88,28 @@ public:
         return ans;
     }
 };
+
+// Method 3: Iterative approach
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root==NULL){
+            return ans;
+        }
+        stack<TreeNode*> st;
+        st.push(root);
+        while(st.size()!=0){
+            root = st.top();
+            st.pop();
+            ans.push_back(root->val);
+            if(root->right!=NULL){
+                st.push(root->right);
+            }
+            if(root->left!=NULL){
+                st.push(root->left);
+            }
+        }
+        return ans;
+    }
+};
