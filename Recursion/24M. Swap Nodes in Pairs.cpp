dca2c298a1b2ad50d578bda88d37c77e473918fc
yshiyi <s268yang@uwaterloo.ca>
2021-04-30 00:49:51 -0400

    Create 24M. Swap Nodes in Pairs.cpp

---
+++ Recursion/24M. Swap Nodes in Pairs.cpp
@@ -0,0 +1,43 @@
+/*
+24. Swap Nodes in Pairs
+
+Description:
+Given a linked list, swap every two adjacent nodes and return its head.
+
+Example 1:
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+
+Example 2:
+Input: head = []
+Output: []
+
+Example 3:
+Input: head = [1]
+Output: [1]
+*/
+
+// This is a recursive solution.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode *head2 = head->next, *tmp = head->next->next;
+        head2->next = head;
+        head->next = swapPairs(tmp);
+        
+        return head2;
+    }
+};
