227e24c1de73652b312756d671ade2ff649d4198
yshiyi <s268yang@uwaterloo.ca>
2021-04-30 00:44:03 -0400

    Create 344. Reverse String.cpp

---
+++ Recursion/344. Reverse String.cpp
@@ -0,0 +1,36 @@
+/*
+344. Reverse String
+Two Pointers, String
+
+Description:
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+Example 1:
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+Constraints:
+1 <= s.length <= 105
+s[i] is a printable ascii character.
+*/
+
+// This is a solution using recursive approach.
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        helper(s, 0, s.size()-1);
+        return;
+    }
+    void helper(vector<char>& str, int left, int right){
+        cout << left << " " << right << endl;
+        if(left>right){
+            return;
+        }
+        swap(str[left], str[right]);
+        helper(str, ++left, --right);
+    }
+};
