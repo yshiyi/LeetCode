56469b31d14ba16ffc369299230d84ec9fe93383
yshiyi <s268yang@uwaterloo.ca>
2021-04-30 11:24:04 -0400

    Update and rename Array/509. Fibonacci Number.py to Recursion/509. Fibonacci Number.py

--- Recursion/509. Fibonacci Number.py
+++ Recursion/509. Fibonacci Number.py
@@ -6,10 +6,9 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n==0:
-            return 0
-        if n==1:
-            return 1
+        if n<2:
+            Solution.m[n] = n
+            return n
         if n-1 not in Solution.m:
             Solution.m[n-1] = self.fib(n-1)
         if n-2 not in Solution.m:
