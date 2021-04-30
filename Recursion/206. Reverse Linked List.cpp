de5a7f2f95312e375ebad1eb46e259c08c3c1a83
yshiyi <s268yang@uwaterloo.ca>
2021-04-30 10:10:25 -0400

    Create 206. Reverse Linked List.cpp

---
+++ Recursion/206. Reverse Linked List.cpp
@@ -0,0 +1,33 @@
+// This is a recursive solution.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL){
+            return head;
+        }
+        ListNode *head2 = new ListNode(0);
+        head2 = recurReverse(head, head2);
+        return head2->next;
+    }
+    ListNode* recurReverse(ListNode* head, ListNode* head2){
+        if(head==NULL){
+            return head;
+        }
+        ListNode* temp1 = head->next;
+        ListNode* temp2 = head2->next;
+        head2->next = head;
+        head->next = temp2;
+        recurReverse(temp1, head2);
+        return head2;
+    }
+};
