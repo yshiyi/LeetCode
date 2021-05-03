1060eeca4183191511504fcf3ef7aae854fb3218
yshiyi <s268yang@uwaterloo.ca>
2021-05-02 23:34:35 -0400

    Update 50M. Pow(x, n).py

--- Recursion/50M. Pow(x, n).py
+++ Recursion/50M. Pow(x, n).py
@@ -1,3 +1,4 @@
+# Method 1: Recursive approach
 class Solution(object):
     def myPow(self, x, n):
         """
@@ -20,3 +21,22 @@ class Solution(object):
         else:
             return 1.0/helper(x, abs(n))
         
+
+"""
+Method 2: Iterative approach
+          This doesn't work in C++, because it will exceed the limit of int when n is super large.
+          Note: every number can be represented as a combination of even powers.
+                e.g. x^5 = x^1 * x^4, x^9 = x^1 + x^8 
+"""
+class Solution(object):
+    def myPow(self, x, n):
+        res = 1.0
+        if n<0:
+            x = 1/x
+            n = -n
+        while n:
+            if n%2==1:
+                res *= x
+            x *= x
+            n /= 2
+        return res
