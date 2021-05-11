127cbdf697bfb6e319aa337f4255a401cc8cbafd
yshiyi <s268yang@uwaterloo.ca>
2021-05-11 16:14:13 -0400

    Create 77M. Combinations.cpp

---
+++ Recursion/77M. Combinations.cpp
@@ -0,0 +1,90 @@
+/*
+77M. Combinations
+Backtracking
+
+Description:
+Given two integers n and k, return all possible combinations of k numbers out of the range [1, n].
+You may return the answer in any order.
+
+Example 1:
+Input: n = 4, k = 2
+Output:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+
+Example 2:
+Input: n = 1, k = 1
+Output: [[1]]
+
+Constraints:
+1 <= n <= 20
+1 <= k <= n
+
+Similar Questions:
+Combination Sum - Medium
+Permutations - Medium
+*/
+// Method 1:
+class Solution {
+public:
+    vector<vector<int>> res;
+    set<set<int>> res_s;
+    vector<vector<int>> combine(int n, int k) {
+        set<int> ans;
+        backtrack(ans, 1, n, k);
+        return res;
+    }
+    void backtrack(set<int>&ans, int start, int end, int k){
+        if(ans.size()==k){
+            if(res_s.find(ans)==res_s.end()){
+                res_s.insert(ans);
+                vector<int> ans_;
+                for(auto v:ans){
+                    ans_.push_back(v);
+                }
+                res.push_back(ans_);
+            }
+            return;
+        }
+        for(int i=start; i<=end; i++){
+            if(ans.find(i)==ans.end()){
+                ans.insert(i);
+                backtrack(ans, i+1, end, k);
+                ans.erase(i);
+            }
+        }
+    }
+};
+
+// Method 2:
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> ans;
+        vector<int> check(n+1);
+        backtrack(ans, check, 1, n, k);
+        return res;
+    }
+    void backtrack(vector<int>&ans, vector<int>& check, int start, int end, int k){
+        if(ans.size()==k){
+            res.push_back(ans);
+            return;
+        }
+        for(int i=start; i<=end; i++){
+            if(check[i]==0){
+                ans.push_back(i);
+                check[i] = 1;
+                backtrack(ans, check, i+1, end, k);
+                ans.pop_back();
+                check[i] = 0;
+            }
+        }
+    }
+};
