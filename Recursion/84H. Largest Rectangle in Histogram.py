368af04f1f171964c2bd4a6570dd5fe45aabee63
yshiyi <s268yang@uwaterloo.ca>
2021-05-14 20:41:12 -0400

    Create 84H. Largest Rectangle in Histogram.py

---
+++ Recursion/84H. Largest Rectangle in Histogram.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        res = 0
+        st = collections.deque()
+        heights.append(0)
+        for i in range(len(heights)):
+            while len(st) and heights[st[-1]] >= heights[i]:
+                cur = st[-1]
+                st.pop()
+                if len(st)==0:
+                    length = i
+                else:
+                    length = i - st[-1] - 1
+                area = heights[cur] * length
+                res = max(res, area)
+            st.append(i)
+        return res
