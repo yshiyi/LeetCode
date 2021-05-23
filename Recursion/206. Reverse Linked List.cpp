92f0d58fc4d474d68ee0cf8f3747299d10a486e3
yshiyi <s268yang@uwaterloo.ca>
2021-05-23 01:22:12 -0400

    Update 206. Reverse Linked List.cpp

--- Recursion/206. Reverse Linked List.cpp
+++ Recursion/206. Reverse Linked List.cpp
@@ -31,3 +31,17 @@ public:
         return head2;
     }
 };
+
+// Method 2:
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        ListNode* last = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return last;
+    }
+};
