8b3c92f0382cc2f612ba91296e7f7331eff47646
yshiyi <s268yang@uwaterloo.ca>
2021-05-27 10:39:29 -0400

    Update 704. Binary Search.py

--- Binary Search/704. Binary Search.py
+++ Binary Search/704. Binary Search.py
@@ -1,3 +1,4 @@
+# Method 1: Recursive approach
 class Solution(object):
     def search(self, nums, target):
         """
@@ -18,3 +19,21 @@ class Solution(object):
             
         return binarySearch(nums, target, 0, len(nums)-1)
         
+# Method 2: Iterative approach
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = int((right+left)/2)
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1
