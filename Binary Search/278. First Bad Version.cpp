b419def723eeb2dc7e68905c5da6e8dbcbb2d7a3
yshiyi <s268yang@uwaterloo.ca>
2021-05-27 21:58:07 -0400

    Update 278. First Bad Version.cpp

--- Binary Search/278. First Bad Version.cpp
+++ Binary Search/278. First Bad Version.cpp
@@ -39,13 +39,13 @@ Guess Number Higher or Lower - Easy
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int left = 0, right = n;
+        int left = 1, right = n;
         while(left <= right){
             int mid = left + (right-left)/2;
             if(!isBadVersion(mid)){
                 left = mid + 1;
             }else{
-                if(mid==0){
+                if(mid==1){
                     return mid;
                 }
                 if(!isBadVersion(mid-1)){
