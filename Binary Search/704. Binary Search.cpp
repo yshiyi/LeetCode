e44cbc64fe29afed6b4027259c79580b5e71ccdb
yshiyi <s268yang@uwaterloo.ca>
2021-05-31 00:31:57 -0400

    Update 704. Binary Search.cpp

--- Binary Search/704. Binary Search.cpp
+++ Binary Search/704. Binary Search.cpp
@@ -72,3 +72,29 @@ public:
         }
     }
 };
+
+// Template III
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size()-1;
+        while(left+1< right){
+            int mid = left + (right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]>target){
+                right = mid;
+            }else{
+                left = mid;
+            }
+        }
+        if(nums[left]==target){
+            return left;
+        }else if(nums[right]==target){
+            return right;
+        }else{
+            return -1;
+        }
+    }
+};
