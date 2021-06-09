f23b8914f6c2ccb2827e8b63a59657dc14d9f243
yshiyi <s268yang@uwaterloo.ca>
2021-06-09 16:28:06 -0400

    Create 35. Search Insert Position.py

---
+++ Binary Search/35. Search Insert Position.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums)
+        while left < right:
+            mid = (left + right)//2
+            if nums[mid]==target:
+                return mid
+            if nums[mid]>target:
+                right = mid
+            else:
+                left = mid + 1
+        return left
