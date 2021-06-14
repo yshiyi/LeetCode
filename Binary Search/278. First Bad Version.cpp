4a00fb24187412edf8b5ce4fc5765c991c353cfa
yshiyi <s268yang@uwaterloo.ca>
2021-06-14 00:19:53 -0400

    Update 278. First Bad Version.cpp

--- Binary Search/278. First Bad Version.cpp
+++ Binary Search/278. First Bad Version.cpp
@@ -95,3 +95,23 @@ public:
         return -1;
     }
 };
+
+/*
+Method 3: Shorter version
+*/
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while(left < right){
+            int mid = left + (right-left)/2;
+            if(!isBadVersion(mid)){
+                left = mid+1;
+            }else{
+                right = mid;
+            }
+        }
+        if(left==right && isBadVersion(left)){return left;}
+        return -1;
+    }
+};
