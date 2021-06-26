49821bb5c9ef7256534b38ca345b569de49585ff
yshiyi <s268yang@uwaterloo.ca>
2021-06-25 20:28:38 -0400

    Create 695M. Max Area of Island.cpp

---
+++ Recursion/695M. Max Area of Island.cpp
@@ -0,0 +1,67 @@
+/*
+695M. Max Area of Island
+
+Description:
+You are given an m x n binary matrix grid. 
+An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) 
+You may assume all four edges of the grid are surrounded by water.
+The area of an island is the number of cells with a value 1 in the island.
+Return the maximum area of an island in grid. If there is no island, return 0.
+
+Example 1:
+Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+               [0,0,0,0,0,0,0,1,1,1,0,0,0],
+               [0,1,1,0,1,0,0,0,0,0,0,0,0],
+               [0,1,0,0,1,1,0,0,1,0,1,0,0],
+               [0,1,0,0,1,1,0,0,1,1,1,0,0],
+               [0,0,0,0,0,0,0,0,0,0,1,0,0],
+               [0,0,0,0,0,0,0,1,1,1,0,0,0],
+               [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Output: 6
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+
+Example 2:
+Input: grid = [[0,0,0,0,0,0,0,0]]
+Output: 0
+
+Constraints:
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 50
+grid[i][j] is either 0 or 1.
+
+Similar Question:
+Number of Islands -Medium
+Island Perimeter - Easy
+Largest Submatrix With Rearrangements - Medium
+*/
+
+/*
+Method: Similar to 79M. Word Search
+        Try each single cell as the start point. Save the maximum area in a global variable.
+*/
+class Solution {
+public:
+    vector<vector<int>> dirs{{0,-1},{-1,0},{0,1},{1,0}};
+    int res = 0;
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] != 1) continue;
+                int cnt = 0;
+                helper(grid, i, j, cnt);
+            }
+        }
+        return res;
+    }
+    void helper(vector<vector<int>>& grid, int i, int j, int& cnt) {
+        int m = grid.size(), n = grid[0].size();
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] <= 0) return;
+        res = max(res, ++cnt);
+        grid[i][j] *= -1;
+        for (auto dir : dirs) {
+            helper(grid, i + dir[0], j + dir[1], cnt);
+        }
+    }
+};
