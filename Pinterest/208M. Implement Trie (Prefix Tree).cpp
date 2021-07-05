2d4e86f07843597cc76b8e2f6c87af268aece4b8
yshiyi <s268yang@uwaterloo.ca>
2021-07-05 11:44:29 -0400

    Update 208M. Implement Trie (Prefix Tree).cpp

--- Pinterest/208M. Implement Trie (Prefix Tree).cpp
+++ Pinterest/208M. Implement Trie (Prefix Tree).cpp
@@ -1,12 +1,18 @@
 /*
 208M. Implement Trie (Prefix Tree)
 Description:
-A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store 
+and retrieve keys in a dataset of strings. 
+There are various applications of this data structure, such as autocomplete and spellchecker.
 Implement the Trie class:
 Trie() Initializes the trie object.
 void insert(String word) Inserts the string word into the trie.
-boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
-boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), 
+and false otherwise.
+boolean startsWith(String prefix) Returns true if there is a previously inserted string word that 
+has the prefix prefix, and false otherwise.
+
+
 Example 1:
 Input
 ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
@@ -21,10 +27,14 @@ trie.search("app");     // return False
 trie.startsWith("app"); // return True
 trie.insert("app");
 trie.search("app");     // return True
+
+
 Constraints:
 1 <= word.length, prefix.length <= 2000
 word and prefix consist only of lowercase English letters.
 At most 3 * 104 calls in total will be made to insert, search, and startsWith.
+
+
 Simliar Questions:
 Design Add and Search Words Data Structure - Medium
 Design Search Autocomplete System - Hard
