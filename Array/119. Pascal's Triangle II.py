e55c4b4998a3ac6a063b9a0b2cc892717adbff13
yshiyi <s268yang@uwaterloo.ca>
2022-03-20 21:45:03 -0400

    Update 119. Pascal's Triangle II.py

--- Array/119. Pascal's Triangle II.py
+++ Array/119. Pascal's Triangle II.py
@@ -1,3 +1,6 @@
+'''
+Method 1:
+'''
 class Solution(object):
     def getRow(self, rowIndex):
         """
@@ -12,3 +15,30 @@ class Solution(object):
                 ans[j] = ans[j] + ans[j-1]
         
         return ans
+        
+'''
+Method 2: Recursive approach
+'''
+Pascal = defaultdict(list)
+class Solution(object):
+    
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        if rowIndex == 0:
+            Pascal[rowIndex] = [1]
+            return [1]
+        elif rowIndex == 1:
+            Pascal[rowIndex] = [1, 1]
+            return [1, 1]
+        row = [0]*(rowIndex+1)
+        row[0], row[-1] = 1, 1
+        for i in range(1,len(row)-1,1):
+            if rowIndex-1 not in Pascal:
+                self.getRow(rowIndex-1)
+            row[i] = Pascal[rowIndex-1][i-1] + Pascal[rowIndex-1][i]
+        Pascal[rowIndex] = row
+        return row
+        
