c38b9a3dd2146ac23bb99e2c6f0ef19ee2c0bb26
yshiyi <s268yang@uwaterloo.ca>
2022-03-29 02:02:06 -0400

    Update 23H. Merge k Sorted Lists.py

--- Merge Sorted/23H. Merge k Sorted Lists.py
+++ Merge Sorted/23H. Merge k Sorted Lists.py
@@ -1,12 +1,45 @@
 '''
+23. Merge k Sorted Lists
 
+Description:
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+Merge all the linked-lists into one sorted linked-list and return it.
 
+Example 1:
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+
+Example 2:
+Input: lists = []
+Output: []
+
+Example 3:
+Input: lists = [[]]
+Output: []
+
+Constraints:
+k == lists.length
+0 <= k <= 104
+0 <= lists[i].length <= 500
+-104 <= lists[i][j] <= 104
+lists[i] is sorted in ascending order.
+The sum of lists[i].length will not exceed 104.
 '''
 
 # Solution:
 '''
 Method: Create a pointer in each list, and use a prioriy queue to save the values
         Pop the smallest one, and save it to the new list
+        Time complexity: O(N logk)
+        Space complexity: O(N) for the new list, O(k) for the queue
 '''
 
 # Definition for singly-linked list.
