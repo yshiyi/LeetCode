686606df83684c11e387d846c999f11493b1a491
yshiyi <s268yang@uwaterloo.ca>
2022-03-29 21:46:35 -0400

    Create 56M. Merge Intervals.py

---
+++ Merge Interval/56M. Merge Intervals.py
@@ -0,0 +1,47 @@
+'''
+56. Merge Intervals
+
+Description:
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, 
+and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+Example 1:
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
+Example 2:
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+Constraints:
+1 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 104
+'''
+
+# Solution
+'''
+Method: Sort the list according to the first element
+        Put the first interval into the ans
+        Compare the ending time of the last element in ans to the starting time of the next interval in intervals
+          If overlapping, update the ending time of the last element in ans
+          else put the interval into the ans
+'''
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals.sort()
+        ans = []
+        ans.append(intervals[0])
+        for i in range(1, len(intervals)):
+            if ans[-1][1] < intervals[i][0]:
+                ans.append(intervals[i])
+            else:
+                ans[-1][1] = max(ans[-1][1], intervals[i][1])
+        return ans
+        
