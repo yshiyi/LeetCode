1a4d02b6dc845b68f812759b5d3eea3eaec476c8
yshiyi <s268yang@uwaterloo.ca>
2022-03-30 02:32:13 -0400

    Update and rename 953. Verifying an Alien Dictionary,py to 953. Verifying an Alien Dictionary.py

--- Topological sort/953. Verifying an Alien Dictionary.py
+++ Topological sort/953. Verifying an Alien Dictionary.py
@@ -35,7 +35,18 @@ All characters in words[i] and order are English lowercase letters.
 """
 
 """
+Time complexity : O(M). M is the total number of characters in words.
+Storing the letter-order relation of each letter takes O(N) time. 
+For the nested for-loops, we examine each pair of words in the outer-loop and for the inner loop, 
+we check each letter in the current word. Therefore, we will iterate over all of letters in words.
 
+Taking both into consideration, the time complexity is O(M + N). 
+However, we know that N is fixed as 26. Therefore, the time complexity is O(M).
+
+Space complexity : O(1). 
+The only extra data structure we use is the hashmap that serves to store the letter-order relations 
+for each word in order. 
+Because the length of order is fixed as 26, this approach achieves constant space complexity.
 """
 class Solution(object):
     def isAlienSorted(self, words, order):
