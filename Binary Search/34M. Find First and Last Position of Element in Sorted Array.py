bc4ee7d8e032b08cac5ad2e9a7b4f726fc0b58bc
yshiyi <s268yang@uwaterloo.ca>
2022-03-31 21:34:58 -0400

    Update 34M. Find First and Last Position of Element in Sorted Array.py

--- Binary Search/34M. Find First and Last Position of Element in Sorted Array.py
+++ Binary Search/34M. Find First and Last Position of Element in Sorted Array.py
@@ -5,6 +5,44 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        """
+        Approach 1:
+        """
+        ans = [-1, -1]
+        if len(nums)<1:
+            return ans
+        left, right = 0, len(nums)
+        while left <right:
+            mid = (left+right)//2
+            if nums[mid]<target:
+                left = mid+1
+            else:
+                right = mid
+        if left!=len(nums) and nums[left]==target:
+            ans[0] = left
+        else:
+            return ans
+        
+        left, right = 0, len(nums)
+        while left<right:
+            mid=(left+right)//2
+            if nums[mid]>target:
+                right = mid
+            else:
+                left = mid+1
+        if nums[right-1]==target:
+            ans[1] = right-1
+        return ans
+        
+        
+        
+        
+        
+        
+        
+        """
+        Approach 2:
+        """
         res = [-1, -1]
         if len(nums)==0:
             return res
