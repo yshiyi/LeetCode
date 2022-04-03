49f351de8128579a4aa08556e89328ac19856fca
yshiyi <s268yang@uwaterloo.ca>
2022-04-03 01:11:06 -0400

    Create 124H. Binary Tree Maximum Path Sum.py

---
+++ Recursion/124H. Binary Tree Maximum Path Sum.py
@@ -0,0 +1,57 @@
+"""
+124. Binary Tree Maximum Path Sum
+
+Description:
+A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. 
+A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+The path sum of a path is the sum of the node's values in the path.
+Given the root of a binary tree, return the maximum path sum of any non-empty path.
+ 
+
+Example 1:
+      1
+     2 3 
+Input: root = [1,2,3]
+Output: 6
+Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
+
+Example 2:
+        -10
+      9     20
+          15  7
+Input: root = [-10,9,20,null,null,15,7]
+Output: 42
+Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 3 * 104].
+-1000 <= Node.val <= 1000
+"""
+
+# Solution:
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxPathSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.ans = float('-inf')
+        self.dfs(root)
+        return self.ans
+    
+    def dfs(self, node):
+        if node is None: return 0
+        l = self.dfs(node.left)
+        r = self.dfs(node.right)
+        
+        self.ans = max(self.ans, max(l,0)+max(r,0)+node.val)
+        return max(l,r,0)+node.val
+        
