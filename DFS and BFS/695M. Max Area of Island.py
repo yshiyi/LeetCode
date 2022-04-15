dcfcb5b959bd50cfd9e9792dd2cd1b2faefb0ddc
yshiyi <s268yang@uwaterloo.ca>
2022-04-15 02:35:46 -0400

    Create 695M. Max Area of Island.py

---
+++ DFS and BFS/695M. Max Area of Island.py
@@ -0,0 +1,64 @@
+"""
+695. Max Area of Island
+
+Description:
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally 
+(horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+The area of an island is the number of cells with a value 1 in the island.
+Return the maximum area of an island in grid. If there is no island, return 0.
+ 
+
+Example 1:
+Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+               [0,0,0,0,0,0,0,1,1,1,0,0,0],
+               [0,1,1,0,1,0,0,0,0,0,0,0,0],
+               [0,1,0,0,1,1,0,0,1,0,1,0,0],
+               [0,1,0,0,1,1,0,0,1,1,1,0,0],
+               [0,0,0,0,0,0,0,0,0,0,1,0,0],
+               [0,0,0,0,0,0,0,1,1,1,0,0,0],
+               [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Output: 6
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+
+Example 2:
+Input: grid = [[0,0,0,0,0,0,0,0]]
+Output: 0
+ 
+
+Constraints:
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 50
+grid[i][j] is either 0 or 1.
+"""
+
+# Solution:
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        max_area = 0
+        r, c = len(grid), len(grid[0])
+        for i in range(r):
+            for j in range(c):
+                if grid[i][j]==0:
+                    continue
+                area = 0
+                q = collections.deque()
+                q.append((i, j))
+                while len(q):
+                    x, y = q.popleft()
+                    grid[x][y]=0
+                    area += 1
+                    for d in dirs:
+                        new_x = x + d[0]
+                        new_y = y + d[1]
+                        if new_x<0 or new_x>=r or new_y<0 or new_y>=c or grid[new_x][new_y]==0:
+                            continue
+                        grid[new_x][new_y]=0
+                        q.append((new_x, new_y))
+                max_area = max(max_area, area)
+        return max_area
