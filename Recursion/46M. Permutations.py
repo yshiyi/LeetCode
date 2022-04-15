1e24aaf7426bcb105950d4d24d61bb76e7b7b076
yshiyi <s268yang@uwaterloo.ca>
2022-04-14 22:34:45 -0400

    Update 46M. Permutations.py

--- Recursion/46M. Permutations.py
+++ Recursion/46M. Permutations.py
@@ -19,3 +19,25 @@ class Solution(object):
                     
         backtrack(nums, temp)
         return res
+    
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        self.ans = []
+        num = []
+        self.helper(nums, num)
+        return self.ans
+    
+    def helper(self, nums, num):
+        if len(num)==len(nums):
+            self.ans.append(copy.deepcopy(num))
+            return
+        for n in nums:
+            if n in num:
+                continue
+            num.append(n)
+            self.helper(nums, num)
+            num.pop()
+        
