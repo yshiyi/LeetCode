96d80ca9a3d884c87b194959c2c21f384ff0e500
yshiyi <s268yang@uwaterloo.ca>
2022-04-14 22:31:54 -0400

    Update 90M. Subsets II.py

--- Recursion/90M. Subsets II.py
+++ Recursion/90M. Subsets II.py
@@ -27,3 +27,29 @@ class Solution(object):
                     check[i]=0
         backtrack(nums, res, temp, check, 0)
         return res
+    
+    def subsetsWithDup(self, nums):
+        self.ans = [[]]
+        tmp = []
+        check = [0]*len(nums)
+        self.helper(sorted(nums), 0, check, tmp)
+        return self.ans
+    
+    def valid(self, nums, check, i):
+        if check[i]==1:
+            return False
+        elif i>0 and nums[i]==nums[i-1] and check[i-1]==0:
+            return False
+        return True
+    
+    def helper(self, nums, start, check, tmp):
+        if start==len(nums):
+            return
+        for i in range(start, len(nums)):
+            if self.valid(nums, check, i):
+                tmp.append(nums[i])
+                self.ans.append(copy.deepcopy(tmp))
+                check[i]=1
+                self.helper(nums, i+1, check, tmp)
+                check[i]=0
+                tmp.pop()
