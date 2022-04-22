1c65534faa04201f73ddbc40fbf9ad28cea6e6b0
yshiyi <s268yang@uwaterloo.ca>
2022-04-22 02:55:51 -0400

    Update Simplified XML Validator.py

--- Amazon/Simplified XML Validator.py
+++ Amazon/Simplified XML Validator.py
@@ -36,3 +36,58 @@ Output
 	false
 12 test cases total, 3 showed and 9 hidden.
 """
+
+import collections
+class Solution(object):
+    def validXML(self, text):
+        stack = collections.deque()
+        i = 0
+        while i<len(text):
+            if text[i]=="<":
+                valid, tag = self.helper(text[i:])
+                if not valid:
+                    return False
+		#tags cannot be empty
+                if tag=="" or tag=="/":
+                    return False
+		#If we find a closing tag </abc>
+                if tag[0]=="/":
+		    #if stack is empty, that means there was no opening tag, return false
+                    if len(stack)==0:
+                        return False
+		    #if stack top is 'abc' but our closing tag is 'xyz', return False
+                    if stack[-1]!=tag[1:]:
+                        return False
+		    #if there is a match to our closing tag then pop it from stack
+                    stack.pop()
+                else:
+		     #if this is the opening tag then add it to stack
+                    stack.append(tag)
+		#we increment i to tag length plus 2 to add <> to the legth of i
+                i += len(tag)+2
+            elif text[i]==">":
+		#if there is a random > in text then return false
+                return False
+            else:
+                i += 1
+        if len(stack):
+	    #if we never found a closing tag to one of our opening tags
+            return False
+        return True
+    
+    def helper(self, text):
+        tag = ""
+        for i in range(1, len(text)):
+            if text[i]=="<":
+                #text = <<abc>
+                return False, -1
+            if text[i]==">":
+		#Completes the tag <abc>
+                return True, tag
+            tag += text[i]
+        return True, tag
+
+
+sol = Solution()
+text = "<a>text<b>other text</b></b>"
+print(sol.validXML(text))
