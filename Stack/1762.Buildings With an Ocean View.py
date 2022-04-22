268e21eaeee4c1107e8ddd6f03e40e893c2e1d42
yshiyi <s268yang@uwaterloo.ca>
2022-04-22 15:27:40 -0400

    Create 1762.Buildings With an Ocean View.py

---
+++ Stack/1762.Buildings With an Ocean View.py
@@ -0,0 +1,45 @@
+"""
+1762. Buildings With an Ocean View
+Description:
+There are n buildings in a line. You are given an integer array heights of size n that represents the 
+heights of the buildings in the line.
+The ocean is to the right of the buildings. A building has an ocean view if the building can see the ocean 
+without obstructions. Formally, a building has an ocean view if all the buildings to its right have a smaller height.
+Return a list of indices (0-indexed) of buildings that have an ocean view, sorted in increasing order.
+ 
+Example 1:
+Input: heights = [4,2,3,1]
+Output: [0,2,3]
+Explanation: Building 1 (0-indexed) does not have an ocean view because building 2 is taller.
+Example 2:
+Input: heights = [4,3,2,1]
+Output: [0,1,2,3]
+Explanation: All the buildings have an ocean view.
+Example 3:
+Input: heights = [1,3,2,4]
+Output: [3]
+Explanation: Only building 3 has an ocean view.
+Example 4:
+Input: heights = [2,2,2,2]
+Output: [3]
+Explanation: Buildings cannot see the ocean if there are buildings of the same height to its right.
+ 
+Constraints:
+1 <= heights.length <= 105
+1 <= heights[i] <= 109
+"""
+
+class Solution(object):
+    def findBuildings(self, heights):
+        stack = collections.deque()
+        for index, B in enumerate(heights):
+            if len(stack)==0 or stack[-1][1] > B:
+                stack.append((index, B))
+            else:
+                while len(stack) and stack[-1][1] <= B:
+                    stack.pop()
+                stack.append((index, B))
+        res = []
+        for index, B in stack:
+            res.append(index)
+        return res
