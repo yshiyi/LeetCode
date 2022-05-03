3f951a4038420dccd584a2b529c31bd5c2263862
yshiyi <s268yang@uwaterloo.ca>
2022-05-02 21:31:17 -0400

    Create Count.py

---
+++ Nuro/Count.py
@@ -0,0 +1,10 @@
+"""
+input: “test”
+
+output: [“test”, “1est”, “t1st”, “te1t”, “tes1”, 
+         “2st”, “t2t”, “te2", 
+         “1e1t”, “1es1", “t1s1”, 
+         “1e2", “2s1”, 
+         “3t”, “t3”, 
+         “4"]
+"""
