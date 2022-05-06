9c9edf7fe06e275e34cc59716217d6dfd57ab24f
yshiyi <s268yang@uwaterloo.ca>
2022-05-06 01:13:21 -0400

    Create Point in max overlapping intervals.py

---
+++ Nuro/Point in max overlapping intervals.py
@@ -0,0 +1,14 @@
+"""
+Given number M and N intervals in the form [a, b] (inclusive) where for every interval -M <= a <= b <= M, create a program that returns a point where the maximum number of intervals overlap.
+
+Example:
+
+M: 10
+N: 4
+Intervals:
+[-3, 5]
+[0, 2]
+[8, 10]
+[6, 7]
+A correct answer would be either 0 ,1 or 2 since those points are found where 2 intervals overlap and 2 is the maximum number of overlapping intervals.
+"""
