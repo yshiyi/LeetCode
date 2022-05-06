2e8fff15cd3e9bb55761d5accdaca01e33e8c431
yshiyi <s268yang@uwaterloo.ca>
2022-05-06 01:23:24 -0400

    Update Count.py

--- Nuro/Count.py
+++ Nuro/Count.py
@@ -7,4 +7,15 @@ output: [“test”, “1est”, “t1st”, “te1t”, “tes1”,
          “1e2", “2s1”, 
          “3t”, “t3”, 
          “4"]
+
+public List<String> conversionCombination(String s) {
+        List<String> ans = new ArrayList<>();
+        for (int len = 0; len < s.length(); len++) {
+            for (int i = 0; i + len < s.length(); i++) {
+                ans.add(s.substring(0,i)+String.valueOf(len+1)+s.substring(i+len+1));
+            }
+        }
+        return ans;
+    }
+
 """
