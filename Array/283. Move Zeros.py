4435e19f32cc2078b49cc9f1e5cbbd5ea134156e
yshiyi <s268yang@uwaterloo.ca>
2023-03-23 22:44:21 -0400

    Update 283. Move Zeros.py

--- Array/283. Move Zeros.py
+++ Array/283. Move Zeros.py
@@ -41,3 +41,27 @@ class Solution(object):
                 count += 1
         return nums
                 
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) < 2:
+            return nums
+        slow = 0
+        while slow < len(nums):
+            if nums[slow] == 0:
+                break
+            slow += 1
+        if slow + 1 == len(nums):
+            return nums
+        fast = slow + 1
+        while fast < len(nums):
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1
+            fast += 1
+        return nums
+        
