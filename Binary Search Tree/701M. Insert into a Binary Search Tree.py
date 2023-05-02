8eb54c6443a813b690e6dd1e8443c2ce2957e6c1
yshiyi <s268yang@uwaterloo.ca>
2023-05-01 23:00:19 -0400

    Create 701M. Insert into a Binary Search Tree.py

---
+++ Binary Search Tree/701M. Insert into a Binary Search Tree.py
@@ -0,0 +1,41 @@
+"""
+701M. Insert into a Binary Search Tree.py
+
+Description:
+You are given the root node of a binary search tree (BST) and a value to insert into the tree. 
+Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+
+Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+Constraints:
+The number of nodes in the tree will be in the range [0, 104].
+-108 <= Node.val <= 108
+All the values Node.val are unique.
+-108 <= val <= 108
+It's guaranteed that val does not exist in the original BST.
+"""
+
+"""
+Method: Remember the basic structure of the code for BST:
+        if not root:
+          return
+        if root.val > val:
+          traverse(root.left)
+        if root.val < val:
+          traverse(root.right)
+"""
+class Solution(object):
+    def insertIntoBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return TreeNode(val)
+        if root.val > val:
+            root.left = self.insertIntoBST(root.left, val)
+        if root.val < val:
+            root.right = self.insertIntoBST(root.right, val)
+        return root
+
