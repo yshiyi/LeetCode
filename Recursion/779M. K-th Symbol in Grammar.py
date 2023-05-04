b86edf67ba0f8f47f1a26adc0b87d489e99e9d39
yshiyi <s268yang@uwaterloo.ca>
2023-05-04 01:02:17 -0400

    Update 779M. K-th Symbol in Grammar.py

--- Recursion/779M. K-th Symbol in Grammar.py
+++ Recursion/779M. K-th Symbol in Grammar.py
@@ -1,4 +1,27 @@
 # This is a recursive approach
+"""
+If n is 1, the only element in the row is 0, so we return 0. 
+Otherwise, we recursively find the value of the k-th symbol in the n-1 row. 
+Let mid be the middle index of the n-1 row, which is 2^(n-2). 
+If k is less than or equal to mid, then the k-th symbol in the n row is the same as the k-th symbol in the n-1 row. 
+Otherwise, the k-th symbol in the n row is the complement of the k-mid-th symbol in the n-1 row.
+"""
+class Solution(object):
+    def kthGrammar(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        if n==1:
+            return 0
+        mid = 2**(n-2)
+        if k <= mid:
+            return self.kthGrammar(n-1, k)
+        else:
+            return 1 - self.kthGrammar(n-1, k - mid)
+
+
 class Solution(object):
     def kthGrammar(self, N, K):
         """
